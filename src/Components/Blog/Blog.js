31b26df90b24d9d314c202002b3dfb7a01e640c9
lubna201 <lubnasazida@gmail.com>
2021-06-15 09:45:47 +0600

    done

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -8,7 +8,7 @@ const Blog = () => {
             <section className="cards-wrapper">
                 <div className="card-grid-space">
                     <div className="num">01</div>
-                    <a className="card" target="_blank" href="https://lubnasazida.medium.com/javascript-b2b711b4885e" id="id1">
+                    <a className="card" target="_blank" href="https://lubnasazida.medium.com/javascript-b2b711b4885e" rel="noopener noreferrer" id="id1">
                         <div>
                             <h1 className="ps-3 pt-3 text-dark">Javascript</h1>
                             <div className="ps-3 text-dark"><h3>Click To read the blog</h3></div>
@@ -17,7 +17,7 @@ const Blog = () => {
                 </div>
                 <div className="card-grid-space mt-5">
                     <div className="num">02</div>
-                    <a className="card img-fluid" target="_blank" href="https://lubnasazida.medium.com/react-project-deploy-on-netlify-without-connecting-to-git-a643a6f28558" id="id2">
+                    <a className="card img-fluid" target="_blank" href="https://lubnasazida.medium.com/react-project-deploy-on-netlify-without-connecting-to-git-a643a6f28558" rel="noopener noreferrer" id="id2">
                         <div>
                             <h1 className="ps-3 pt-3 text-dark">React Project deploy on Netlify</h1>
                             <div className="ps-3 text-dark"><h3>Click To read the blog</h3></div>
@@ -26,7 +26,7 @@ const Blog = () => {
                 </div>
                 <div className="card-grid-space">
                     <div className="num">03</div>
-                    <a className="card" target="_blank" href="https://lubnasazida.medium.com/little-things-about-javascript-6cd0ec24bfb" id="id3">
+                    <a className="card" target="_blank" href="https://lubnasazida.medium.com/little-things-about-javascript-6cd0ec24bfb" rel="noopener noreferrer" id="id3">
                         <div>
                         <h1 className="ps-3 pt-3 text-dark">Little things about Javascript</h1>
                             <div className="ps-3 text-dark"><h3>Click To read the blog</h3></div>
